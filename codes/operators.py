1ab81f9d9d0f80ef3aefe945f280c2d1b3a3b89d
MohitBehl <mohitbehlofficial@gmail.com>
2019-01-05 14:10:16 +0530

    operators.py added

---
+++ codes/operators.py
@@ -0,0 +1,26 @@
+num1 = int(input("enter first number:"));
+num2 = int(input("enter second number:"));
+print("num1: ",num1," num2: ",num2,"\n\n");
+
+print("ARITHMETIC OPERATORS:");
+print("+,-,*,/,//,%,**");
+print("(addition) num1 + num2 :",num1+num2);
+print("(subtraction) num1 - num2 :",num1-num2);
+print("(multiplication) num1 * num2 :",num1*num2);
+print("(division) num1 / num2 :",num1/num2);
+print("(integer-division) num1 // num2 :",num1//num2);
+print("(exponentiation) num2 ** 2 :",num2**2,"\n\n");
+
+print("RELATIONAL OPERATORS:");
+print("<,>,<=,>=,==,!=");
+print("10 < 2 :",10<2);
+print("15 > 5 :",15>5);
+print("20 <= 15 :",20<=15);
+print("5 == 5 :",5 == 5);
+print("0 != 1 :",0!=1,"\n\n");
+
+print("LOGICAL OPERATORS:");
+print("not,and,or");
+print("not(12 < 6):",not(12<6));
+print("(15<5 and 5 < 15):",(15<5 and 5 < 15));
+print("(15<5 or 5 < 15):",(15<5 or 5 < 15),"\n\n");
